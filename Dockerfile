89977aa15150bba91cf383627a2cc532a8ef4206
SleepyHead <53241974+techroy23@users.noreply.github.com>
2025-06-14 06:12:46 +0800

    Update

--- Dockerfile
+++ Dockerfile
@@ -3,13 +3,23 @@ FROM debian:latest
 ENV DEBIAN_FRONTEND=noninteractive
 
 RUN apt-get update -y && apt-get install -y \
-    curl wget tar htop net-tools tcpdump \
+    curl wget tar htop net-tools \
     && apt-get autoclean -y && apt-get autoremove -y && apt-get autopurge -y && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
 
-COPY custom.sh /custom.sh
-RUN bash custom.sh
-
+# Set up working directory
 WORKDIR /app
+
+# Download EarnApp installation script
+RUN wget --verbose --output-document=/app/setup.sh https://brightdata.com/static/earnapp/install.sh
+
+# Extract version from installation script
+RUN VERSION=$(grep VERSION= /app/setup.sh | cut -d'"' -f2) && \
+    wget --verbose --output-document=/usr/bin/earnapp "https://cdn-earnapp.b-cdn.net/static/earnapp-x64-$VERSION" && \
+    chmod -R a+rwx /usr/bin/earnapp
+
+# Copy custom script and entrypoint script
+COPY custom.sh /custom.sh
+RUN bash /custom.sh
 
 COPY entrypoint.sh /entrypoint.sh
 RUN chmod +x /entrypoint.sh
--- entrypoint.sh
+++ entrypoint.sh
@@ -17,73 +17,29 @@ if [[ -z "$EARNAPP_UUID" ]]; then
 fi
 
 echo "### ### ### ### ### ### ### ### ###"
-echo " Starting custom.sh ... "
+echo " Running custom.sh (if present) ... "
 echo "### ### ### ### ### ### ### ### ###"
 if [ -f "/custom.sh" ]; then
     chmod -R a+rwx /custom.sh
     bash /custom.sh
+    GETINFO="
+    ##### lsb_release #####
+    $(lsb_release -a 2>/dev/null)
+
+    ##### hostnamectl #####
+    $(hostnamectl 2>/dev/null)
+    "
+
+    echo "$GETINFO"
+    echo " "
 else
     echo "Skipping custom.sh as it is not present."
 fi
 echo " "
 
-GETINFO="
-##### lsb_release #####
-$(lsb_release -a 2>/dev/null)
-
-##### hostnamectl #####
-$(hostnamectl 2>/dev/null)
-"
-
-echo "$GETINFO"
-echo " "
-
 echo "### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ###"
-echo " Downloading EarnApp installation script to get the latest version of the binary ... "
-echo "### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ###"
-
-wget --verbose --output-document=/app/setup.sh https://brightdata.com/static/earnapp/install.sh
-if [[ $? -ne 0 ]]; then
-    echo "Error: Failed to download EarnApp installation script."
-    exit 1
-fi
-
-ARCH=$(uname -m)
-VERSION=$(grep VERSION= /app/setup.sh | cut -d'"' -f2)
-
-if [[ -z "$VERSION" ]]; then
-    echo "Error: VERSION could not be determined."
-    exit 1
-fi
-
-if [[ -z "$ARCH" ]]; then
-    echo "Error: ARCH could not be determined."
-    exit 1
-fi
-
-echo "Found version $VERSION"
-echo " "
-case $ARCH in
-    x86_64|amd64) filename="earnapp-x64-$VERSION" ;;
-    armv7l|armv6l) filename="earnapp-arm7l-$VERSION" ;;
-    aarch64|arm64) filename="earnapp-aarch64-$VERSION" ;;
-    *) filename="$PRODUCT-arm7l-$VERSION" ;;  # Default fallback
-esac
-
-echo "### ### ### ### ### ### ###"
-echo " Download EarnApp binary "
-echo "### ### ### ### ### ### ###"
-echo " "
-
-wget --verbose --output-document=/usr/bin/earnapp "https://cdn-earnapp.b-cdn.net/static/$filename"
-if [[ $? -ne 0 ]]; then
-    echo "Error: Failed to download EarnApp binary."
-    exit 1
-fi
-
-echo "### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ###"
 echo " Setting up directory, status file, UUID and permissions ... "
-echo "### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ###"
+echo "### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ###"
 echo " "
 mkdir -p /etc/earnapp
 touch /etc/earnapp/status
